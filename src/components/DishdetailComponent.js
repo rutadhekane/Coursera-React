db120fbce3da44ca788d374e874bb97a18d3d75c
Ruta Dhekane <rutad57@gmail.com>
2020-12-17 15:35:50 -0800

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
 import { Link } from'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -113,7 +114,7 @@ class CommentForm extends Component {
 		if(dish!=null) {
 			return (
 				<Card>
-					<CardImg top src={dish.image} alt={dish.name} />
+					<CardImg top src={baseUrl + dish.image} alt={dish.name} />
 						<CardBody>
 							<CardTitle>{dish.name}</CardTitle>
 							<CardText>{dish.description}</CardText>
